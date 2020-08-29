832dc68457d312cbc5027626c048256091e8e57e
your-Jackson <your>
2020-08-29 19:58:11 +1200

    updated director.py

--- domainmodel/director.py
+++ domainmodel/director.py
@@ -11,28 +11,17 @@ class Director:
     def director_full_name(self) -> str:
         return self.__director_full_name
 
-    def __repr__(self):
-        return f"<Director {self.__director_full_name}>"
+    def __repr__(self) -> str:
+        return "<Director {}>".format(self.__director_full_name)
 
-    def __eq__(self, other):
-        # TODO
-        pass
+    def __eq__(self, other:'Director') -> bool:
+        return self.__director_full_name == other.__director_full_name
 
-    def __lt__(self, other):
-        # TODO
-        pass
+    def __lt__(self, other:'Director') -> bool:
+        return self.__director_full_name < other.__director_full_name
+
 
     def __hash__(self):
-        # TODO
-        pass
+        return hash(self.__director_full_name)
 
 
-class TestDirectorMethods:
-
-    def test_init(self):
-        director1 = Director("Taika Waititi")
-        assert repr(director1) == "<Director Taika Waititi>"
-        director2 = Director("")
-        assert director2.director_full_name is None
-        director3 = Director(42)
-        assert director3.director_full_name is None
