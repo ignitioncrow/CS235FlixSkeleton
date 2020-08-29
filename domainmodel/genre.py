39b513a83eaa98b130ed70a1584c4db3a2360b30
your-Jackson <your>
2020-08-29 19:58:47 +1200

    updated genre.py

--- domainmodel/genre.py
+++ domainmodel/genre.py
@@ -1,3 +1,19 @@
 
 class Genre:
-    pass
+    def __init__(self, genre_name: str):
+        if genre_name == "" or type(genre_name) is not str:
+            self.__genre_name = None
+        else:
+            self.__genre_name = genre_name.strip()
+    @property
+    def genre_name(self) -> str:
+        return self.__genre_name
+    def __repr__(self) -> str:
+        return "<Genre {}>".format(self.__genre_name)
+    def __eq__(self, other: 'Genre') -> bool:
+        return self.__genre_name == other.__genre_name
+    def __lt__(self, other: 'Genre') -> bool:
+        return self.__genre_name < other.__genre_name
+    def __hash__(self):
+        return hash(self.__genre_name)
+
