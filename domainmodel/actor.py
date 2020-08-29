17a136c2388ee7b78c8b899dca1a33221dfc44a7
your-Jackson <your>
2020-08-29 19:57:19 +1200

    updated actor.py

--- domainmodel/actor.py
+++ domainmodel/actor.py
@@ -1,4 +1,34 @@
 
 class Actor:
-    pass
+    def __init__(self, actor_full_name: str):
+        self.__actor_colleague_li = []
+        if actor_full_name == "" or type(actor_full_name) != str:
+            self.__actor_full_name = None
+        else:
+            self.__actor_full_name = actor_full_name.strip()
+    @property
+    def actor_full_name(self)->str:
+        return self.__actor_full_name
+    @actor_full_name.setter
+    def actor_full_name(self, other: str):
+        self.__actor_full_name = other
+    def __repr__(self)->str:
+        return "<Actor {}>".format(self.__actor_full_name)
+    def __eq__(self, other: 'Actor')->bool:
+        return self.__actor_full_name == other.__actor_full_name
+    def __lt__(self, other:'Actor')->bool:
+        return self.__actor_full_name < other.__actor_full_name
+    def __hash__(self):
+        return hash(self.__actor_full_name)
+    def add_actor_colleague(self, colleague:'Actor'):
+        if isinstance(colleague,Actor):
+            self.__actor_colleague_li.append(colleague)
+        else:
+            raise TypeError
+    def check_if_this_actor_worked_with(self, colleague:'Actor')->bool:
+        if isinstance(colleague, Actor) and colleague in self.__actor_colleague_li:
+            return True
+        else:
+            return False
+
 
