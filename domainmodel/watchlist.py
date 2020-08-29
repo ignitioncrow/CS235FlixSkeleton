30e39a414823499471ceb4f72dfc3784b95e4b69
your-Jackson <your>
2020-08-29 20:01:12 +1200

    updated extension part

--- domainmodel/watchlist.py
+++ domainmodel/watchlist.py
@@ -1,4 +1,44 @@
 from domainmodel.movie import Movie
 
 class WatchList:
-    pass
+    def __init__(self):
+        self.__movie_li = []
+    def add_movie(self, movie:'Movie'):
+        if isinstance(movie, Movie):
+            if movie not in self.__movie_li:
+                self.__movie_li.append(movie)
+            else:
+                pass
+        else:
+            raise TypeError
+    def remove_movie(self, movie: 'Movie'):
+        if isinstance(movie, Movie):
+            if movie in self.__movie_li:
+                self.__movie_li.remove(movie)
+            else:
+                pass
+        else:
+            raise TypeError
+    def select_movie_to_watch(self, index: int):
+        if index >= 0 and index < len(self.__movie_li):
+            return self.__movie_li[index]
+        else:
+            return None
+    def size(self):
+        return len(self.__movie_li)
+    def first_movie_in_watchlist(self):
+        if len(self.__movie_li) == 0:
+            return None
+        return self.__movie_li[0]
+    def __iter__(self):
+        self.__index = 0
+        return self
+    def __next__(self):
+        if self.__index > self.size() - 1:
+            raise StopIteration
+        else:
+            self.__index += 1
+            return self.__movie_li[self.__index-1]
+
+
+
